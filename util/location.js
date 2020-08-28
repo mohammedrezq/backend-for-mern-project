878b95399e1c0d8b8106c462e98b62da4892361e
Mohammed Rezq <mohammedrezq2000@gmail.com>
2020-08-28 05:58:09 +0200

    Backend First Commit

---
+++ util/location.js
@@ -0,0 +1,10 @@
+const API_KEY = ""; // GOOGLE API KEY which we don't have yet.
+
+const getCoordsForAddress = (address) => {
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516,
+  };
+};
+
+module.exports = getCoordsForAddress;
